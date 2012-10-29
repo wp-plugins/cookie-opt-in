e43a6d4553df7df23217b59e36f4abc3b51226c8
clearsite <clearsite@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-29 08:32:37 +0000

    Upload of version 1.3.0
    git-svn-id: https://plugins.svn.wordpress.org/cookie-opt-in/tags/1.3.0@618531 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp_cookie_opt_in_skin.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: Cookie-Opt-In - Example skin
+Plugin URI: http://wordpress.clearsite.nl
+Description: Example skin for the Cookie_opt_in plugin
+Version: 1.3.0
+Author: Clearsite Webdesigners | Remon Pel
+Author URI: http://clearsite.nl/author/rmpel
+*/
+if (!class_exists('CookieOptIn')) {
+  require_once ('wp_cookie_opt_in.php');
+}
+
+add_filter('do_not_load_cookie_opt_in_visual_effects', 'wp_cookie_opt_in_cls_no_visuals');
+function wp_cookie_opt_in_cls_no_visuals() {
+  return true;
+}
+
+add_filter('init', 'wp_cookie_opt_in_cls_init');
+function wp_cookie_opt_in_cls_init() {
+  if (!is_admin()) {
+    wp_enqueue_script('cookie-opt-in-if', plugins_url('js/cls-cookie-opt-in-if.js', __FILE__), array('jquery', 'cookie-opt-in'), $ver = 1, $in_footer = true);
+    wp_enqueue_style('cookie-opt-in', plugins_url('css/cls-style.css', __FILE__));
+  }
+}
+
+add_filter('cookie_opt_in_settings', 'wp_cookie_opt_in_settings');
+function wp_cookie_opt_in_settings($settings) {
+  $settings['view_details'] = __('View details', 'cookie_opt_in');
+  return $settings;
+}
