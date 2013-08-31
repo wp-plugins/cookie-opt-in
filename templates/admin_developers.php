5a45f8ab0266f947ed4d3c01358b8d688f20d46b
clearsite <clearsite@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-31 07:16:47 +0000

    remove old trunk - sorry, we don't work on trunk here. We have our own development environment and only post the wordpress version here as tags
    git-svn-id: https://plugins.svn.wordpress.org/cookie-opt-in/trunk@765034 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/admin_developers.php
+++
@@ -1,4 +0,0 @@
-<?php
-  if (function_exists('__')) $file = __('en.admin_developers.php', 'cookie_opt_in');
-  if (function_exists('t')) $file = t('en.admin_developers.php');
-  include($file);
--- templates/admin_info.php
+++
@@ -1,4 +0,0 @@
-<?php
-  if (function_exists('__')) $file = __('en.admin_info.php', 'cookie_opt_in');
-  if (function_exists('t')) $file = t('en.admin_info.php');
-  include($file);
